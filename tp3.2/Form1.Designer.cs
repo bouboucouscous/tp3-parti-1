49ec86178433a84ba73f8fd581a158da322bdc7c
bouboucouscous <remi.coussypro@gmail.com>
2024-09-27 16:00:16 +0200

    Ajoutez des fichiers projet.

---
+++ tp3.2/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace tp3._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Service web";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(21, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 20);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Service WCF";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(21, 289);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 20);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Service Rest";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 29);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(475, 53);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(125, 101);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(475, 165);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(125, 289);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(475, 91);
+            this.textBox3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 390);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+
+        #endregion
+    }
+}
+
---
+++ tp3.2/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using tp3._2.WCFMeteo;
+
+using System.Net;
+using System.IO;
+
+namespace tp3._2
+{
+    
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WSMeteo.meteo3ilSoapClient service = new WSMeteo.meteo3ilSoapClient("meteo3ilSoap");
+            string Date, Desc;
+            service.Get_Value(1, out Date, out Desc);
+
+            textBox1.Text =
+                $"Date : {Date} \r\n" +
+                $"temp : {Desc}";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Service1Client sr = new Service1Client();
+
+            MeteoData toto = sr.Get_MeteoData();
+            
+            textBox2.Text =
+                $"Temp : {toto.d_Temp} \r\n" +
+                $"Pres : {toto.d_Pres} \r\n" +
+                $"preci : {toto.d_PrecJour} \r\n" +
+                $"relevé a  : {toto.dt_Releve} \r\n" +
+                $"Hum : {toto.i_Hum}";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string url = "http://www.meteorestservice.lab3il.fr/ServiceRest.svc/meteo/1";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            request.Method = WebRequestMethods.Http.Get;
+            WebResponse response = request.GetResponse();
+            StreamReader streamReader = new StreamReader(response.GetResponseStream());
+            string str = streamReader.ReadLine();
+
+            str = str.Substring(str.IndexOf(">") + 1, str.Length - str.IndexOf(">") - 1);
+            str = str.Substring(0, str.IndexOf("<"));
+
+            textBox3.Text = $"Température : {str}°";
+        }
+    }
+}
